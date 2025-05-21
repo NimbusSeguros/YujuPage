1986c4c6f082b9ae61d19e7a2eeba10693a2ca62
NimbusSeguros <169058931+NimbusSeguros@users.noreply.github.com>
2025-05-21 15:24:46 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   server: {
+    host: '0.0.0.0', // Acepta conexiones externas (IPv4)
+    port: 5173,      // Puerto correcto
+    strictPort: true, // Evita que Vite cambie el puerto automáticamente
+  },
   plugins: [react()],
 })
